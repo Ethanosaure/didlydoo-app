6ca0a5e7e481b88a0efa523922b1c441a81664fb
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-25 13:44:04 +0200

    removing unecessary functions

--- frontend/js/add-event.js
+++ frontend/js/add-event.js
@@ -8,16 +8,3 @@ export async function API_test() {
       throw new Error(erro);
     });
 }
-
-// export function add_event() {
-//   const event_input_value = document.querySelector("#text_input").value;
-//   const description_input_value =
-//     document.querySelector("#description_input").value;
-//   const date_input = document.querySelector("#date_input").value;
-//   const date_input_value = new Date(date_input);
-
-//   if (!event_input_value || !description_input_value || !date_input_value) {
-//     alert("no event to add");
-//     return;
-//   }
-// }
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,4 @@
 import { API_test } from "./add-event.js";
-// import { add_event } from "./add-event.js";
 
 const button = document.querySelector("#add_event_btn");
 
