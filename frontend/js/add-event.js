907fd50b3d5fd77463e4ed4f9c7339295320ef33
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-26 09:58:41 +0200

    adding button

--- frontend/js/add-event.js
+++ frontend/js/add-event.js
@@ -1,11 +1,16 @@
 export function add_event() {
   const section = document.createElement("section");
+  const enter_form = document.createElement("h2");
+  enter_form.innerText = "Fill the blank";
   const author = document.createElement("input");
   author.setAttribute("type", "text");
   author.setAttribute("placeholder", "author");
   const title = document.createElement("input");
   title.setAttribute("type", "text");
   title.setAttribute("placeholder", "title");
+  const validate = document.createElement("button");
+  validate.setAttribute("type", "button");
+  validate.innerText = "Add";
 
   const description = document.createElement("input");
   description.setAttribute("type", "text");
@@ -13,11 +18,11 @@ export function add_event() {
   const date = document.createElement("input");
   date.setAttribute("type", "date");
 
+  section.appendChild(enter_form);
   section.appendChild(author);
   section.appendChild(title);
   section.appendChild(description);
-
   section.appendChild(date);
-
+  section.appendChild(validate);
   document.body.appendChild(section);
 }
