dcc3347c1d1f2efef65b1edb8327c2fcedf8d50a
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-26 09:53:29 +0200

    adding new event

--- frontend/js/add-event.js
+++ frontend/js/add-event.js
@@ -1,10 +1,23 @@
-export async function API_test() {
-  await fetch("http://localhost:5000/api/events/")
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
-    })
-    .catch((erro) => {
-      throw new Error(erro);
-    });
+export function add_event() {
+  const section = document.createElement("section");
+  const author = document.createElement("input");
+  author.setAttribute("type", "text");
+  author.setAttribute("placeholder", "author");
+  const title = document.createElement("input");
+  title.setAttribute("type", "text");
+  title.setAttribute("placeholder", "title");
+
+  const description = document.createElement("input");
+  description.setAttribute("type", "text");
+  description.setAttribute("placeholder", "decription");
+  const date = document.createElement("input");
+  date.setAttribute("type", "date");
+
+  section.appendChild(author);
+  section.appendChild(title);
+  section.appendChild(description);
+
+  section.appendChild(date);
+
+  document.body.appendChild(section);
 }
--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,6 +1,7 @@
 import { first_event } from "./single-event.js";
+import { add_event } from "./add-event.js";
 
 await first_event();
 const button = document.querySelector("#add_event_btn");
 
-// button.addEventListener("click", );
+button.addEventListener("click", () => add_event());
