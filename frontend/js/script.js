83e74cbcfb353c044df86061ef7fd6e62334d92e
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-26 09:34:34 +0200

    adding first_event function

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,5 +1,6 @@
 import { first_event } from "./single-event.js";
 
+await first_event();
 const button = document.querySelector("#add_event_btn");
 
-button.addEventListener("click", async () => await first_event());
+// button.addEventListener("click", );
