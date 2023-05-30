d88dee8f61f50799a5bf91cd4e87deb26196be7c
Ethanosaure <ethandiasmarques.pro@gmail.com>
2023-05-30 11:28:12 +0200

    adding more dates

--- frontend/js/add-event.js
+++ frontend/js/add-event.js
@@ -1,9 +1,10 @@
-// import { validation } from "./validation.js";
+import { adding_more_date } from "./adding_more_date.js";
 import { POST_fetch } from "./function-POST.js";
 import { show_dialog } from "./show-dialog.js";
 export function add_event() {
   show_dialog();
-
+  const add_more_date = document.querySelector("#add_more_date");
+  add_more_date.addEventListener("click", adding_more_date);
   const add_btn = document.querySelector("#add_btn");
   add_btn.addEventListener("click", function (event) {
     event.preventDefault();
