cdf34bbfda422d4b436156bcd9eb3aef80c8ba76
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-30 11:26:11 +0200

    Add condition in add-events

--- frontend/js/add-event.js
+++ frontend/js/add-event.js
@@ -1,12 +1,24 @@
-// import { validation } from "./validation.js";
 import { POST_fetch } from "./function-POST.js";
 import { show_dialog } from "./show-dialog.js";
-export function add_event() {
-  show_dialog();
 
+const author = document.querySelector("#dialog_author");
+const title = document.querySelector("#dialog_title");
+const description = document.querySelector("#dialog_description");
+const date = document.querySelector("#dialog_date");
+
+
+
+export function add_event() {
   const add_btn = document.querySelector("#add_btn");
   add_btn.addEventListener("click", function (event) {
     event.preventDefault();
   });
-  add_btn.addEventListener("click", POST_fetch);
+  add_btn.addEventListener("click", async () =>{
+  if (!title.value || !author.value || !description.value || !date.value) {
+    alert('Please, write else I kill you !');
+    return;
+  } 
+  await POST_fetch();
+});
 }
+show_dialog();
