5596d084f80805e68092b972ada827d14ef3554b
DelphineLecorney <Lecorney.Delphine@gmail.com>
2023-05-30 16:45:58 +0200

    Add guest-function finished

--- frontend/js/adding-guest-function.js
+++ frontend/js/adding-guest-function.js
@@ -1,36 +1,36 @@
 const array = [];
-export async function add_guest(eventId, attendees_input) {
-  await fetch(`http://localhost:5000/api/events/${eventId}`)
-    .then((res) => res.json())
-    .then((json) => {
-      json.dates.forEach((element) => {
-        const newAttend = {
-          date: element.date,
-          available: null,
-        };
-        array.push(newAttend);
-      });
-      console.log(array[0].date);
-    })
-    .catch((err) => console.log(err));
 
-  await fetch(`http://localhost:5000/api/events/${eventId}/attend/`, {
-    method: "POST",
-    headers: {
-      "Content-type": "application/json; charset=UTF-8",
-    },
-    body: JSON.stringify({
-      name: "Gaston",
-      dates: [
-        {
-          date: new Date(array[0].date).toISOString(),
-          available: boolean(false),
-        },
-      ],
-    }),
-  })
-    .then(() => console.log("yes"))
-    .catch((err) => {
-      throw new Error(err);
+export async function add_guest(eventId, attendees_input) {
+  try {
+    const response = await fetch(`http://localhost:5000/api/events/${eventId}`);
+    const json = await response.json();
+    
+    json.dates.forEach((element) => {
+      const newAttend = {
+        date: element.date,
+        available: null,
+      };
+      array.push(newAttend);
     });
+    
+    console.log(array[0].date);
+
+    const postResponse = await fetch(`http://localhost:5000/api/events/${eventId}/attend/`, {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+      body: JSON.stringify({
+        name: attendees_input.value,
+        dates: array.map((item) => ({
+          date: new Date(item.date).toISOString(),
+          available: false,
+        })),
+      }),
+    });
+    
+  } catch (err) {
+    console.log(err);
+    throw new Error(err);
+  }
 }
